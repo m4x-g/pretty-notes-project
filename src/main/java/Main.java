50f72f998bb58d6aa7a1a5a673de634880b63cd4
maxx <gajevskij.maksim@gmail.com>
2021-04-03 08:21:19 +0300

    added a second user in Main 😅

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         User firstUserEver = new User("Astartes");
+        User secondUserEver = new User("Batman");
+
         Note noteOne = firstUserEver.newUserNote("40k", "https://www.youtube.com/watch?v=O7hgjuFfn3A", "40k", "Warhammer");
-        noteOne.printNote();
         noteOne.noteCategories.addCategory("Space Marine");
         noteOne.printNote();
+
+        Note noteTwo = secondUserEver.newUserNote("I am Batman", "Because I'm Batman", "Batman", "Dark Knight");
+        noteTwo.printNote();
     }
 }
